08a8c144ac31f29385e4ee0b8af538af4295b3b4
Papa-Pedro-13 <62937441+Papa-Pedro-13@users.noreply.github.com>
2024-08-27 23:01:33 +0300

    change sidebar styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,12 @@ import styles from './Home.module.css';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <TransferForm from={'userName'} />
+      <h2 className={styles.headline}>Главная</h2>
+      <TransferForm
+        from={'userName'}
+        headline='Перевести собственные коины'
+        max={20000}
+      />
     </div>
   );
 };
